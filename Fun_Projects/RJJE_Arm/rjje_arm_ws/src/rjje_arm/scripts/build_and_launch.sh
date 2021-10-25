c96cbcce509fcdb8f4c1ce0e92b49f01671064c0
rjia <rjia@accelrobotics.com>
2021-10-25 11:14:36 -0500

    relocated fun projects

--- Fun_Projects/RJJE_Arm/rjje_arm_ws/src/rjje_arm/scripts/build_and_launch.sh
+++
@@ -1,4 +0,0 @@
-# assume we have sourced the workspace
-cd ../urdf
-xacro rjje_arm.xacro > rjje_arm.xacro.urdf
-roslaunch rjje_arm rjje_arm.launch
--- Fun_Projects/RJJE_Arm/rjje_arm_ws/src/rjje_arm/scripts/ssh-tool.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-IP=$(sudo arp-scan -l | grep Raspberry | cut -f 1)
-ssh -YC "pi@${IP}"
-
