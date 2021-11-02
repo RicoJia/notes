7f493d4e484052e83522d0c224acd1b614a53ed4
Rico Jia <rjia@accelrobotics.com>
2021-11-02 11:41:59 -0700

    pickele converter

--- examples/pybind_example/test_rico.py
+++ examples/pybind_example/test_rico.py
@@ -8,23 +8,19 @@ import time
 j = 200
 test_done = False
 
-def pickle_converter_callback(): 
-    global test_done 
-    test_done = True
-
 for i in range(100): 
-    example.pybind_test(pickle_converter_callback)
-    while test_done == False: 
-        pass
-
-    infile = open("pickled_test1.txt",'rb')
-    new_dict = pickle.load(infile)
-    print(new_dict)
-    infile.close()
-
-    infile = open("pickled_test2.txt",'rb')
-    new_dict = pickle.load(infile)
-    print(new_dict)
-    time.sleep(0.1)
+    example.pybind_test()
+    # while test_done == False: 
+    #     pass
+    #
+    # infile = open("pickled_test1.txt",'rb')
+    # new_dict = pickle.load(infile)
+    # print(new_dict)
+    # infile.close()
+    #
+    # infile = open("pickled_test2.txt",'rb')
+    # new_dict = pickle.load(infile)
+    # print(new_dict)
+    # time.sleep(0.1)
 
 
