539072d48d9efeb07e494ac25bead58b698264ef
Rico_Jia <rjia@accelrobotics.com>
2022-01-26 22:20:30 -0800

    new files

---
+++ temp/examples/python_examples/queue.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+# deque is a simple data structure, while Queue.queue is a thread-safe datastructure
+q = deque(maxlen=3)
+q.append('a')
+q.append('b')
+q.append('c')
+q.append('d')
+print(q)    #see only b, c, d
+print(q.popleft())  #see b
