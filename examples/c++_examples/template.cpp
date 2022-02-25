38ba50998a4c7bdefc428c5578b106ea504e51b8
Rico_Jia <rjia@accelrobotics.com>
2022-02-24 16:16:21 -0800

    template cpp

--- examples/c++_examples/template.cpp
+++ examples/c++_examples/template.cpp
@@ -3,41 +3,57 @@
 #include <math.h>
 using namespace std;
 
-template <typename T1, class T2>			// LOL you can use typename and class interchangeably. 
-T2 sum (T1 i, T2 f)
-{
-return (T2)(i+f);
-}
+// template <typename T1, class T2>			// LOL you can use typename and class interchangeably. 
+// T2 sum (T1 i, T2 f)
+// {
+// return (T2)(i+f);
+// }
+//
+// template <class T>
+// T product (T a, T b)				// DETAIL: use () instead of <>. 
+// {return a*b; 					// single type. Remember to return something!!!
+// }
+//
+//
+// template <class T1, class T2>
+// class fruits
+// {
+// T1 apple_num;
+// T2 pear_num;
+// public:
+// fruits(T1 apple, T2 pear):apple_num(apple), pear_num(pear)
+// {}
+// void access() const 
+// {cout<<"Apple is :"<<apple_num;
+//  cout<<"Pear is : "<< pear_num;
+// }
+// }; 		//why not fruta?
+//
+// int main ()
+// {
+// cout<<"sum of 5 and 6.5 is"<<sum(5,6.5)<<endl; 
+// cout<<"sum of 6.5 and 5 is"<<sum(6.5,5)<<endl; 
+//
+// cout<<"the product of 5 and 6 is "<< product(5,6)<<endl;
+// cout<<"the product of 5.5 and 6.7 is "<< product(5.5,6.7)<<endl;
+//
+// fruits<float, int> fruta(4.333,5); 		//!! you need to indicate return tyoe here fruits<float, int>!!!
+// fruta.access(); 
+//
+// return 0;
+// }
+//
 
-template <class T>
-T product (T a, T b)				// DETAIL: use () instead of <>. 
-{return a*b; 					// single type. Remember to return something!!!
-}
+// Test 2 - template specialization
+// primary template
+template<typename T>
+void is_voi(T input){};
 
-template <class T1, class T2>
-class fruits
-{
-T1 apple_num;
-T2 pear_num;
-public:
-fruits(T1 apple, T2 pear):apple_num(apple), pear_num(pear)
-{}
-void access() const 
-{cout<<"Apple is :"<<apple_num;
- cout<<"Pear is : "<< pear_num;
-}
-}; 		//why not fruta?
+// explicit specialization for T = char
+// template<>
+// void is_voi <char> (char input){};
 
-int main ()
-{
-cout<<"sum of 5 and 6.5 is"<<sum(5,6.5)<<endl; 
-cout<<"sum of 6.5 and 5 is"<<sum(6.5,5)<<endl; 
+//equivalent to above, since template arguments can be deducted from the function arguments.
+template<>
+void is_voi(char input){}
 
-cout<<"the product of 5 and 6 is "<< product(5,6)<<endl;
-cout<<"the product of 5.5 and 6.7 is "<< product(5.5,6.7)<<endl;
-
-fruits<float, int> fruta(4.333,5); 		//!! you need to indicate return tyoe here fruits<float, int>!!!
-fruta.access(); 
-
-return 0;
-}
--- examples/python_examples/file_related.py
+++ examples/python_examples/file_related.py
@@ -1,38 +1,68 @@
-# ########### Zip file ###########
-# # writing and reading from zip file
-# from zipfile import ZipFile
-# file_name = "mi_archivo.zip"
-# hello_file = "hello.txt"
-# with ZipFile(file_name, 'w') as f: 
-#     f.writestr(hello_file, "hello".encode("utf-8"))
-#     f.close()
-#
-# # append another file
-# another_file = "another_file.txt"
-# with ZipFile(file_name, 'a') as f: 
-#     f.writestr(another_file, "another file~!!!")
-#     f.close()
-#
-# # print all info and zip content
-# with ZipFile(file_name, 'r') as f: 
-#     for info in f.infolist(): 
-#         if (info.filename == hello_file): 
-#             another_hello_file_content = f.read(hello_file)
-#     f.close()
-#
-# # append a file in the zip under another name
-# another_hello_file = "another_hello_file.txt"
-# with ZipFile(file_name, 'a') as f:
-#     f.writestr(another_hello_file, another_hello_file_content)
-#     f.close()
-#
-#
-# with ZipFile(file_name, 'r') as f: 
-#     for info in f.infolist(): 
-#         print(info.filename)
-#         print(f.read(info.filename))
-#     f.close()
+########### Zip file ###########
+# writing and reading from zip file
+from zipfile import ZipFile
+file_name = "mi_archivo.zip"
+hello_file = "hello.txt"
+with ZipFile(file_name, 'w') as f: 
+    f.writestr(hello_file, "hello".encode("utf-8"))
+    f.close()
 
+# append another file
+another_file = "another_file.txt"
+with ZipFile(file_name, 'a') as f: 
+    f.writestr(another_file, "another file~!!!")
+    f.close()
+
+# print all info and zip content
+with ZipFile(file_name, 'r') as f: 
+    for info in f.infolist(): 
+        if (info.filename == hello_file): 
+            another_hello_file_content = f.read(hello_file)
+    f.close()
+
+# append a file in the zip under another name. Just append, not removing
+another_hello_file = "another_hello_file.txt"
+with ZipFile(file_name, 'w') as f:
+    f.writestr(another_hello_file, another_hello_file_content)
+    f.close()
+
+
+def print_zip(file_name):
+    with ZipFile(file_name, 'r') as f: 
+        for info in f.infolist(): 
+            print(info.filename)
+            print(f.read(info.filename))
+        f.close()
+
+
+
+# update just one file - we have to recompress and delete
+import os
+def update_one_file(src_name, target_name, replacement_content): 
+    """
+    Add or update target in a zip file with replacement_content
+    src_name: zip file to be updated
+    target_name: file inside src_name to be added/replaced
+    replacement_content: string to be substituted in
+    """
+    tmp = src_name+"tmp"
+    with ZipFile(src_name, "r") as infile, ZipFile(tmp, "w") as outfile: 
+        for info in infile.infolist(): 
+            if info.filename != target_name: 
+                outfile.writestr(info.filename, infile.read(info.filename))
+            else: 
+                outfile.writestr(info.filename, replacement_content)
+        infile.close()
+        outfile.close()
+    os.remove(src_name)
+    # os.rename(old_name, new_name)
+    os.rename(tmp, src_name)
+
+print("before updating")
+print_zip(file_name)
+print("now let's update just one file")
+update_one_file(file_name, another_hello_file, "around the world")
+print_zip(file_name)
 
 ########## argparse ###########
 import argparse
@@ -46,20 +76,20 @@ import argparse
 # parser.add_argument('-o', '--output', action='store_true', help=DOC)
 # args = parser.parse_args()
 
-# - adds custom msg
-parser = argparse.ArgumentParser(add_help=False)
-DOC = f"""
-- This script adds snapshots to old spoofs from new spoofs, so the old spoofs can work with the store-geometry-editor
-    - Assume src_dir contains newer spoofs of the exact same store geometry as dst_dir
-    - If the two directories exist, we will append 'requests/GET/snapshot' from each spoof in src_dir, to each spoof in dst_dir
-    """
-# - add positional args, so you can do python file.py SRC DIR, no need for --src_dir
-parser.add_argument("src_dir", type=str, default=None)
-parser.add_argument("dst_dir", type=str, default=None)
-# will show a "flat" version of the DOC, with the usage already
-parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
-                    help=DOC)
-args = parser.parse_args()
+# # - adds custom msg
+# parser = argparse.ArgumentParser(add_help=False)
+# DOC = f"""
+# - This script adds snapshots to old spoofs from new spoofs, so the old spoofs can work with the store-geometry-editor
+#     - Assume src_dir contains newer spoofs of the exact same store geometry as dst_dir
+#     - If the two directories exist, we will append 'requests/GET/snapshot' from each spoof in src_dir, to each spoof in dst_dir
+#     """
+# # - add positional args, so you can do python file.py SRC DIR, no need for --src_dir
+# parser.add_argument("src_dir", type=str, default=None)
+# parser.add_argument("dst_dir", type=str, default=None)
+# # will show a "flat" version of the DOC, with the usage already
+# parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
+#                     help=DOC)
+# args = parser.parse_args()
 
 
 # - exclusive argparse
