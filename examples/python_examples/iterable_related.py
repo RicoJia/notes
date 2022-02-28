f152b205e5c95a3d7b32b572ebd5584bde6b102d
Rico_Jia <rjia@accelrobotics.com>
2022-02-28 14:45:35 -0800

    iterable related

---
+++ examples/python_examples/iterable_related.py
@@ -0,0 +1,61 @@
+import numpy as np
+def kwargs_test(): 
+    def test_args(*args): 
+        print(args) # should see a tuple
+
+    class test_kwargs: 
+        def __init__(self, **kwargs): 
+            print(kwargs)   # should see a dictionary
+            print(kwargs["foo"])
+
+    test_kwargs(foo="bar", love=101)
+    test_args(101, "123")
+     
+
+    # partial
+    from functools import partial
+    def func(a, b): 
+        print(a, b)
+    func_w = partial(func, b = 12)
+    func_w(a = 13)
+
+
+# vidb, ddd, debugger for python 
+
+def np_filter(): 
+    arr = np.ones((4,3))
+    arr[0,:] *= np.NINF
+
+    not_inf = ~np.isinf(arr)   #matrix shows true for elements that're not inf
+    a = not_inf.any(axis=1)     #examine each row? False if any element in the row is false
+    arr[[True,False, False, False]] # shows first row. Different than arr[[1,0,0,0]]?
+    filtered = arr[a]       # the last result we want
+
+
+def dict_to_list():
+    di = {1:"a", 2:"b"}
+    ls = list(di.items())
+    print(list(filter(lambda x: x[0] < 5, ls)))
+    # dict_items() type, not supporting filter() directly on dict.items(). Must use list
+    print(type(di.items()))
+
+def set_funcs(): 
+    s = set()
+    s = {1,2,3}
+    s.add(4)
+    print("s add: ", s)
+    # remove will raise an error if not existing 
+    s.remove(2)
+    # discard will not raise an error
+    s.discard(2)
+    print("s remove and discard", s)
+
+    s2 = {2,3,4}
+    print("s2 n s", s2.intersection(s))
+    print("s2 n s", s2 & s)
+    # shows S2 - (s2 & s)
+    print("s2 - s", s2 - s)
+
+if __name__ == "__main__": 
+    # dict_to_list()
+    set_funcs()
