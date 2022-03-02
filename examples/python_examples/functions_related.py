7083fefa6f3fb212e689f1b050c3186633d3c96b
Rico_Jia <rjia@accelrobotics.com>
2022-03-01 17:37:41 -0800

    nested func

--- examples/python_examples/functions_related.py
+++ examples/python_examples/functions_related.py
@@ -1,6 +1,34 @@
 import numpy as np
 
-arr = np.random.rand(4,3,3)
-print(arr)
-mask = np.logical_and((arr > arr.min()), (arr < arr.max()))
-print(mask)
+def test_nested_func(): 
+    class Foo(object):
+        def __init__(self):
+           self.haha = "mark" 
+        def nested_func(self): 
+            def another_func(): 
+                self.haha = "markhaha"
+            another_func()
+            print(self.haha)
+
+    f = Foo()
+    f.nested_func()
+
+
+def np_masking(): 
+    arr = np.random.rand(4,3,3)
+    print(arr)
+    mask = np.logical_and((arr > arr.min()), (arr < arr.max()))
+    print(mask)
+
+def property_test():
+    class foo: 
+        pass
+        
+    f = foo()
+    f.temperature = 800
+    print(f.temperature)
+
+    print(f.__class__.__name__)
+
+if __name__ == "__main__": 
+    test_nested_func()
--- examples/python_examples/property.py
+++
@@ -1,8 +0,0 @@
-class foo: 
-    pass
-    
-f = foo()
-f.temperature = 800
-print(f.temperature)
-
-print(f.__class__.__name__)
