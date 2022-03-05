341a6b0e84eb7fb7d61d9c408eb6eda36a08e6ba
Rico_Jia <rjia@accelrobotics.com>
2022-03-04 19:09:10 -0800

    crash

--- examples/c++_examples/crash.cpp
+++ examples/c++_examples/crash.cpp
@@ -19,6 +19,16 @@ void make_unique_crash()
    // 3. list initializer should use {}, not ()
    // auto ptr = std::unique_ptr<crash_t>(new crash_t(1,2)); 
    auto ptr = std::unique_ptr<crash_t>(new crash_t{1,2}); 
+
+   // default intiailization itself doesn't cause crash
+   auto default_obj = crash_t(); 
+   default_obj.i = 9;
+   // But this can cause crash, because this is not default ctor, it's just nullptr
+   auto default_ptr = std::unique_ptr<crash_t>(); 
+   // This is the real default ctor using unique_ptr
+   default_ptr = std::unique_ptr<crash_t>(new crash_t()); 
+   if (default_ptr) std::cout<<__FUNCTION__<<": valid default ptr"<<std::endl;
+   default_ptr->i = 9;
 }
 
 #include <mutex>
@@ -27,10 +37,37 @@ void mtx_ptr_in_struct()
     struct crash_t {
         std::mutex* mtx_; 
     };    
+
+    struct Foo {
+        public: 
+            Foo(std::unique_ptr<crash_t>&& ptr){
+                if (ptr->mtx_ != nullptr) std::cout<<__FUNCTION__<<": mtx ptr valid"<<std::endl;
+                std::unique_lock<std::mutex> ul(*(ptr->mtx_)); 
+                std::cout<<__FUNCTION__<<": victory"<<std::endl;
+            }
+        private: 
+            std::unique_ptr<crash_t>ptr_; 
+    };
+    
+    class Bar
+    {
+    public:
+        Bar (): f_(std::unique_ptr<crash_t>(new crash_t{&mtx_}))
+        {
+
+        }
+    
+    private:
+        std::mutex mtx_;
+        Foo f_;
+    };
+
+    // This is fine actually. just remember to move(ptr), if ptr is already an object
+    Bar();
 }
 
 int main()
 {
-    // make_unique_crash();
-    mtx_ptr_in_struct();
+    make_unique_crash();
+    // mtx_ptr_in_struct();
 }
