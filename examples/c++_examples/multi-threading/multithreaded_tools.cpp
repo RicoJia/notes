787072a9822ec6cc7a13707331c25ca81c135f5a
rjia <rjia@accelrobotics.com>
2022-03-06 13:13:39 -0600

    test future exceptions

--- examples/c++_examples/multi-threading/multithreaded_tools.cpp
+++ examples/c++_examples/multi-threading/multithreaded_tools.cpp
@@ -30,6 +30,46 @@ void set_promise(std::promise<int> p){
     p.set_value(12); 
 }
 
+void test_future_exceptions(){
+    // 1. set exception as we wish
+    auto promise_set_exception = [](){
+        std::promise<int> p; 
+        std::future<int> fut = p.get_future();
+        std::thread t1(
+            [&](){
+                p.set_exception(std::make_exception_ptr(std::runtime_error("mine")));
+            }
+                // but a canonical use of std::promise::set_exception is 
+                // try{
+                //   throw std::runtime_error("hehe");
+                // }
+                // catch(...)    //... in C++ will tell compiler NOT to check arg number and types
+                // {
+                //   // exception_ptr current_exception() noexcept;
+                //   p.set_exception(std::current_exception());    // returns the current exception_ptr
+                // }
+                );
+        try{
+            std::cout<<__FUNCTION__<<fut.get()<<std::endl;
+        }
+        catch(...){
+            std::cout<<__FUNCTION__<<": exception comes"<<std::endl;
+        }
+        t1.join(); 
+    }; 
+    promise_set_exception();
+
+    // 2. broken_promise, because promise is destroyed before setting value
+    auto broken_promise_exception = [](){
+        std::future<int> fut;
+        {
+            std::promise<int> p;
+            fut = p.get_future();
+        }
+        fut.get();
+    }; 
+}
+
 void test_future_and_shared_future(){
     // 1. cannonical future
     std::promise<int> sumPromise;
@@ -49,6 +89,8 @@ void test_future_and_shared_future(){
 
     // 3. you can push function accessing the same std::shared_future
     auto func = [&](){
+        // future::wait(): blocks until result becomes available, then get() will have no wait
+        // - ```valid() == true``` (shared_state)
         sf3.wait(); 
         std::cout<<__FUNCTION__<<": sf: "<<sf3.get()<<std::endl;
     }; 
@@ -68,5 +110,6 @@ void test_async(){
 
 int main() {
     // test_packaged_task_basic();
-    test_future_and_shared_future();
+    // test_future_and_shared_future();
+    test_future_exceptions();
 }
