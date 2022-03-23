74e34e248bd4c80dc33557c7aaaa1ab8579fc304
rjia <rjia@accelrobotics.com>
2022-03-23 16:26:19 -0500

    affine matrix

--- examples/ros_examples/src/test_pkg/src/test_tf.cpp
+++ examples/ros_examples/src/test_pkg/src/test_tf.cpp
@@ -1,8 +1,10 @@
+#include <iostream>
 #include <ros/ros.h>
 #include <tf2/LinearMath/Quaternion.h>
-
+#include <Eigen/Dense>
 #include "tf/message_filter.h"
 
+using std::cout; using std::endl; 
 /**
  * Notes:
  *  q.w = cr * cp * cy + sr * sp * sy;
@@ -17,8 +19,32 @@ void quat_calc()
     tf2::Quaternion q; 
 }
 
+/**
+* @brief: This can be used in ROS. https://stackoverflow.com/questions/33117457/conversions-between-tf-and-eigen
+* Great Tutorial: https://github.com/felipepolido/EigenExamples
+* 1. Affine3f is defied as: 
+*  linear translation
+*  0...0  1
+* 2. use Affine3f::matrix() for printing
+*/
+void test_affine()
+{
+    Eigen::Affine3f transform_temp = Eigen::Affine3f::Identity(); 
+    transform_temp.translation() = Eigen::Vector3f(1.0, 1.0, 2.0); 
+    transform_temp.rotate(Eigen::AngleAxisf(M_PI, Eigen::Vector3f(0,0,1)));
+
+    cout<<transform_temp.matrix()<<endl;
+    // transform_temp.linear() = 
+
+    // const Eigen::Matrix3d rotation_part = transform_temp.rotation().cast<double>();
+    // const Eigen::Vector3d translation_part = transform_temp.translation().cast<double>();
+    // tf::Transform transform_tf;
+    // tf::transformEigenToTF(transform_eigen, transform_tf);
+}
+
 int main(int argc, char**argv)
 {
     ros::init(argc, argv, "test_tf"); 
+    test_affine();
     
 }
