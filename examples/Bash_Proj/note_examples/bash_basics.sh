ef4d3add2379c1a435cfccaa5f09c99ffc100c05
rjia <rjia@accelrobotics.com>
2022-03-26 15:01:21 -0500

    bash basics

--- examples/Bash_Proj/note_examples/bash_basics.sh
+++ examples/Bash_Proj/note_examples/bash_basics.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+# must require the shbang, otherwise, we might be running another shell
 # function basics
 NUM1=32
 function float_eval(){
   local FLOAT="$(echo "scale=2; $*" | bc -q 2>/dev/null)"
   echo ${FLOAT}
 }
+
 # res=$(float_eval $NUM1)
 # echo "the result is ${res}"
 
@@ -16,4 +19,18 @@ function delete(){
     # note we have - instead of --. This method is not like xargs rm -rf, which has a max of 1000 something
     find $pwd -type f -name *log -delete
 }
-delete
+# delete
+
+function test_if(){
+    # 1. need spacing after if, before [[. ]]. Spacing matters, else it won't evaluate properly!!
+    if [[ 1 ]]
+    then 
+        echo "hehe"
+    fi
+
+    # - "" is used whenever the variable might contain space or is empty. This way, the entire thing is treated as a single argument. Else, the string might be broken into many. So, always wrap string with ""
+    # - UID is 0 if you execute the script as root, UID is a **shell built-in variable, not an environment variable**.  
+    # -eq for equal, = for comparing strings, == is for bash only. -ne is not equal. != for strings. -le is less than or equal to
+    if [[ ${UID} -eq 0 ]]; then echo "UID is 0"; fi
+}
+test_if
