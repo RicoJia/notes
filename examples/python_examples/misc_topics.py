545b7ce0b3b04f147ce52591557eb95782fda88b
Rico_Jia <rjia@accelrobotics.com>
2022-04-08 11:51:39 -0700

    python str

--- examples/python_examples/misc_topics.py
+++ examples/python_examples/misc_topics.py
@@ -24,8 +24,24 @@ def test_tup():
     a, b = 1, 2
     print(a,b)
 
+def test_string(): 
+    #ljust returns a 20 char long str, with "O" padding char
+    txt = "banana"
+    x = txt.ljust(20, "O")
+    print(x)
+
+    # to split a string into a list of words, based on delim
+    str_ = "lol, lol"
+    # see['lol', ' lol']
+    print(str_.split(','))
+    print(str_.endswith("s"))
+    # in total len(str_) is 15, with * on the sides
+    print(str_.center(15, "*"))
+
+
+
 if __name__=="__main__":
     # test_warning()
-    test_math()
-    test_tup()
-
+    # test_math()
+    # test_tup()
+    test_string()
--- examples/python_examples/time.py
+++ examples/python_examples/time.py
@@ -1,7 +1,21 @@
+#!/usr/bin/python
 import time 
-time_str = time.strftime("%Y, %m, %d")  #returns string that corresponds to the format strings
-print(time_str)
-str_1 = "123"
-str_2 = "123"
-str_3 = "123"
-str_4 = "123"
+def test_time_str(): 
+    time_str = time.strftime("%Y, %m, %d")  #returns string that corresponds to the format strings
+    print(time_str)
+    str_1 = "123"
+    str_2 = "123"
+    str_3 = "123"
+    str_4 = "123"
+
+def test_cpu_time(): 
+    """
+    CPU time (time.clock()) is the time cpu process a task + system calls of the process in total 
+    Wall time (time.time())is the time actually passes, like looking at the time on the wall
+    Wall time - CPU time = intentional sleep, system wait to get a task processed 
+    """
+    print(f"wall time: {time.time()}, cpu time: {time.process_time()}")
+
+if __name__ == "__main__":
+    test_cpu_time()
+
