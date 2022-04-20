0b9a7b1f4e763d149b4d0f3ec0214f890e1f5f20
Rico_Jia <rjia@accelrobotics.com>
2022-04-20 11:20:36 -0700

    updated the repo

--- examples/python_examples/third_party/np_basics.py
+++ examples/python_examples/third_party/np_basics.py
@@ -32,7 +32,16 @@ def invert_arr():
     arr[1,2] = 99
     print(arr[:, ::-1])
 
+def test_flatten(): 
+    arr = np.ones([4,2])
+    arr[2, :] = [1,2]
+    print(arr.flatten())
+    ls = [[1,2], [3,4], [5,6]]
+    print(np.stack(ls))
+    print(np.array(ls))
+
 if __name__=="__main__":
     # test_empty_arr()
     # test_math()
-    invert_arr()
+    # invert_arr()
+    test_flatten()
