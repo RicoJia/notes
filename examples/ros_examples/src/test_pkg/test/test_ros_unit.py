40105eb0d9f7d8929928c8db43f6972520ff9d02
Rico Jia <ricoruotongjia@gmail.com>
2022-10-15 15:58:19 -0500

    rosunit

--- examples/ros_examples/src/test_pkg/test/test_ros_unit.py
+++ examples/ros_examples/src/test_pkg/test/test_ros_unit.py
@@ -5,7 +5,14 @@ import rospy
 import rostest
 from logging import getLogger
 
+# 1. rostest args: http://wiki.ros.org/roslaunch/XML/test No output
+# 2. manually run rostest: http://wiki.ros.org/rostest/Commandline
+
 class MyTestCase(unittest.TestCase):
+    # comment this out if you don't need to start ros topics, etc.
+    # def __init__(self, *args, **kwargs):
+    #     rospy.init_node('test_params')
+
     def test_param_loaded(self):
         value = rospy.get_param('/value', None)
         rospy.logwarn("==============rospy loginfo")
@@ -15,4 +22,5 @@ class MyTestCase(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    
     rostest.rosrun('tutorial', 'test_params', MyTestCase)
