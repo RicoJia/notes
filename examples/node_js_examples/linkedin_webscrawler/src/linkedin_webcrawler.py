1f604981f1ba1a390b4b46b65f639fd458ebcf68
Rico Jia <ricoruotongjia@gmail.com>
2023-07-26 09:23:51 -0500

    small clean up

--- examples/node_js_examples/linkedin_webscrawler/src/linkedin_webcrawler.py
+++ examples/node_js_examples/linkedin_webscrawler/src/linkedin_webcrawler.py
@@ -4,15 +4,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import os
-from bs4 import BeautifulSoup as bs
 import re as re
 import time
-import pandas as pd
-from collections import deque
 
 from pudb import set_trace
 
