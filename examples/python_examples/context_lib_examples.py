08bc05ce0929d8d8931f604bbad549bb58fe4d5b
ricocoyeah <rjia@diligentrobots.com>
2023-10-19 12:18:45 -0500

    context lib example

--- examples/python_examples/context_lib_examples.py
+++ examples/python_examples/context_lib_examples.py
@@ -5,15 +5,33 @@ def test_session_use():
         - context mgmr requires you to have exactly one yield
         - all code before is the setup code
         - all code afterwards is the teardown code
+    Use method scope takes in a method name, then wrap the function under context method, which should be in its own class
     """
+    import functools
+    def use_method_scope(context_method_name):
+        def decorator(func):
+            functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                with context_method_name():
+                    return func(*args, **kwargs)
+            return wrapper
+        return decorator
     class RequestContext:
+        def __init__(self):
+            self.s = None
         @contextlib.contextmanager
         def session(self):
-            if self.s = None:
+            if self.s is None:
                 self.s = {"haha": "haha"}
             yield self.s
 
-    @contextlib.contextmanager
-    def _reuse_request_context(self):
-        with self.request_context.session():
-            yield
+    class Foo:
+        def __init__(self) -> None:
+            self.request_context = RequestContext()
+        @contextlib.contextmanager
+        def _reuse_request_context(self):
+            with self.request_context.session():
+                yield
+        @use_method_scope("_reuse_request_context")
+        def _fetch_cars(self) -> bool:
+            return True
